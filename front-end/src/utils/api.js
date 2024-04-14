05e46fdbae9bc75a6cec2997c4bdd37fa12f31e6
Stephen Walker <35walke774@gmail.com>
2024-04-14 13:36:24 -0400

    updated api base url in api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,8 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-time";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://capstone-project-back.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
